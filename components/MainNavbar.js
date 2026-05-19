66388e88430bc527ddf542d2f2f55a127f803244
Jason Mace <jason.r.mace@gmail.com>
2026-05-19 14:51:05 -0500

    Added white color to toggler

--- components/MainNavbar.js
+++ components/MainNavbar.js
@@ -7,7 +7,7 @@ const MainNavbar = () => {
         <Navbar.Brand href="#home">
             <img src="https://barbizon.com/wp-content/uploads/2022/12/barbizon-logo-22.svg" srcSet="https://barbizon.com/wp-content/uploads/2022/12/barbizon-logo-22.svg 1x,https://barbizon.com/wp-content/uploads/2022/12/barbizon-logo-22.svg 2x,https://barbizon.com/wp-content/uploads/2022/12/barbizon-logo-22.svg 3x" alt="Barbizon Lighting Company" style={{width: "132px"}} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{color: "#ffffff"}} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
