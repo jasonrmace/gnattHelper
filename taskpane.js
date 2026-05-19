936ed800e1507d388f25ffc1e5daaf0e2bcf2e42
Jason Mace <jason.r.mace@gmail.com>
2026-05-19 16:11:00 -0500

    Changed to version 3.4

--- taskpane.js
+++ taskpane.js
@@ -8,7 +8,7 @@ const ProjectList = window.ProjectList;
 
 function App() {
     // --- STATE ---
-    const [version] = useState("React-Bootstrap v3.3");
+    const [version] = useState("React-Bootstrap v3.4");
     const [activeTab, setActiveTab] = useState("ProjectList");
 
     // --- INITIALIZATION ---
