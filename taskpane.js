af23fade2ad495fbb86a0f9614ab91fc28f3e59f
Jason Mace <jason.r.mace@gmail.com>
2026-05-20 10:32:39 -0500

    changed version number to 3.6.1

--- taskpane.js
+++ taskpane.js
@@ -8,7 +8,7 @@ const ProjectList = window.ProjectList;
 
 function App() {
     // --- STATE ---
-    const [version] = useState("React-Bootstrap v3.6");
+    const [version] = useState("React-Bootstrap v3.6.1");
     const [activeTab, setActiveTab] = useState("ProjectList");
 
     // --- INITIALIZATION ---
