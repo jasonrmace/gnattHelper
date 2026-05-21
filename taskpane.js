2b089e3d409ee1dc256fa86fd904a70dc992d02e
Jason Mace <jason.r.mace@gmail.com>
2026-05-21 17:52:16 -0500

    Changed version to 4.0.1

--- taskpane.js
+++ taskpane.js
@@ -14,7 +14,7 @@ const ProjectList = window.ProjectList;
 
 function App() {
     // --- STATE ---
-    const [version] = useState("React-Bootstrap v4.0.0");
+    const [version] = useState("React-Bootstrap v4.0.1");
     const [activeTab, setActiveTab] = useState("ProjectList");
     
     // VALIDATION STATE: Defaults to true, flips to false if check fails
